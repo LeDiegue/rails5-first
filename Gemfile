22fd12cc2ef43d990c4518fcfcdf84bf55e78cc3
Maxi Vadora <maxivadora@gmail.com>
2017-05-03 12:55:05 -0300

    JWT

--- Gemfile
+++ Gemfile
@@ -13,6 +13,9 @@ gem 'active_model_serializers'
 
 gem 'bcrypt', '~> 3.1.7' # for use has_secure_password
 
+#encoding and decoding JWT tokens
+gem 'jwt'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,8 @@ module First
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # the contents of the lib directory have to be included when the Rails applciation loads
+    config.autoload_paths << Rails.root.join('lib')
   end
 end
