06abfb85a85e40cee57c145cb0571ca8a0d16a76
Maxi Vadora <maxivadora@gmail.com>
2017-05-04 15:49:22 -0300

    it was moved authentication_controller into api/v1/ folder

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -1,10 +1,9 @@
-class AuthenticationController < ApplicationController
+class Api::V1::AuthenticationController < ApplicationController
 	skip_before_action :authenticate_request
 
 	def authenticate
 		command = AuthenticateUser.call(params[:email], params[:password])
-
-		if command.succes?
+		if command.success?
 			render json: { auth_token: command.result }
 		else
 			render json: { error: command.errors }, status: :unauthorized
