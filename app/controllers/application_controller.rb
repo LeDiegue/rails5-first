0cbf853b765812a66a52898b09629a99ede0a1c6
Maxi Vadora <maxivadora@gmail.com>
2017-05-04 13:12:56 -0300

    logging user and authorizing request

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::API
-  include ActionController::Serialization
-  #cambio para la pull
+	include ActionController::Serialization
+	
+	before_action :authenticate_request
+	
+	attr_reader :current_user
+
+	private
+
+	def authenticate_request
+		@current_user = AuthorizeApiRequest.call(request.headers).result
+		render json: { error: 'Not Athorized' }, status: 401 unless @current_user
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   scope module: 'api' do
     namespace :v1 do
       get 'hello', to: 'hello#index'
+      post 'authenticate', to: 'authentication#authenticate'
     end
   end
 end
